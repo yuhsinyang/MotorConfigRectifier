43f163d494298e75509aef1b0712a85ad247b24f
yuhsinyang <yuhsin2685@gmail.com>
2021-07-04 21:37:18 +0800

    Deleted reference codes.

--- ParseMotorINI/Util/INIParser.cs
+++ ParseMotorINI/Util/INIParser.cs
@@ -50,18 +50,6 @@ namespace ParseMotorINI.Util
             return s.Split('\0').Select(x => x.Split('=')).Select(x =>
               { return x; }).Where(x=>x.Length==2).ToDictionary(x=>x[0], x=>x[1]);
         }
-        //public List<KeyValuePair<string, string>> IniReadSection(string section)
-        //{
-        //    byte[] buffer = new byte[MAX_BUFFER];
-        //    uint bytesReturned = GetPrivateProfileSection(section, buffer, MAX_BUFFER, iniPath);
-        //    string allKeyValues = new string(Encoding.ASCII.GetChars(buffer), 0, (int)bytesReturned);
-        //    return ToKeyValueList(allKeyValues);
-        //}
-        //static List<KeyValuePair<string, string>> ToKeyValueList(string s)
-        //{
-        //    return s.Split('\0').Select(x => x.Split('=')).Select(x =>
-        //      { return x; }).Where(x=>x.Length==2).Select(x => new KeyValuePair<string, string>(x[0], x[1])).ToList();
-        //}
         public bool IniDeleteSection(string section)
         {
             return WritePrivateProfileSection(section, null, iniPath);
@@ -72,44 +60,10 @@ namespace ParseMotorINI.Util
             uint bytesReturned = GetPrivateProfileSectionNames(buffer, MAX_BUFFER, iniPath);
             return new string(Encoding.ASCII.GetChars(buffer), 0, (int)bytesReturned).Split('\0').ToList();
         }
-        //public string[] IniGetSectionNames()
-        //{
-        //    IntPtr pReturnedString = Marshal.AllocCoTaskMem((int)MAX_BUFFER);
-        //    uint bytesReturned = GetPrivateProfileSectionNames(pReturnedString, MAX_BUFFER, iniPath);
-        //    if (bytesReturned == 0)
-        //        return null;
-        //    string local = Marshal.PtrToStringAnsi(pReturnedString, (int)bytesReturned).ToString();
-        //    Marshal.FreeCoTaskMem(pReturnedString);
-        //    //use of Substring below removes terminating null for split
-        //    return local.Substring(0, local.Length - 1).Split('\0');
-        //}
-        //public void IniGetSectionNames()
-        //{
-        //    byte[] buffer = new byte[MAX_BUFFER];
-        //    uint bytesReturned = GetPrivateProfileSectionNames(buffer, MAX_BUFFER, iniPath);
-        //    List<string> sections = new List<string>();
-        //    var sectionName = new StringBuilder();
-        //    for(int i = 0; i < bytesReturned; i++)
-        //    {
-        //        var c = (char)buffer[i];
-        //        if(c!='\0')
-        //        {
-        //            sectionName.Append(c);
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine(sectionName);
-        //            sectionName.Clear();
-        //        }
-        //    }
-
-        //}
         [DllImport("kernel32")]
         private static extern long WritePrivateProfileString(string section, string key,string val,string filePath);
         [DllImport("kernel32")]
         private static extern int GetPrivateProfileString(string section, string key,string def, StringBuilder retVal, int size,string filePath);
-        //[DllImport("kernel32")]
-        //private static extern uint GetPrivateProfileSectionNames(IntPtr pszReturnBuffer, uint nSize, string lpFileName);
         [DllImport("kernel32")]
         private static extern uint GetPrivateProfileSectionNames(byte[] pszReturnBuffer, uint nSize, string lpFileName);
         [DllImport("kernel32.dll")]
